8fd997d27a2a0cd9c34f096bf9db9251ebd5f43a
Dan Lynn <dan@danlynn.org>
2017-06-06 15:15:04 -0400

    Initial source commit

---
+++ bin/profilelog.js
@@ -0,0 +1,131 @@
+'use strict';
+
+/**
+ * Simple profile logging utility
+ *
+ * usage:
+ *   const ProfileLog = require('./profilelog')
+ *   profileLog = new ProfileLog('Test Performance')
+ *   let start = Date.now()
+ *   ...do something being measured...
+ *   profileLog.addEntry(start, 'Did something slow')
+ *   start = Date.now()
+ *   ...do something being measured again...
+ *   profileLog.addEntry(start, 'Did something slow again')
+ *   profileLog.writeToConsole()
+ *
+ * output:
+ *   Test Performance:
+ *      11185: Did something slow
+ *       2089: Did something slow again
+ */
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ProfileLog = function () {
+  function ProfileLog() {
+    var title = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Execution Profiling Log:";
+    var enabled = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+
+    _classCallCheck(this, ProfileLog);
+
+    this.title = title;
+    this.enabled = enabled;
+    this.entries = [];
+    this.groups = {};
+  }
+
+  /**
+   * Clear the profiling info entries
+   */
+
+
+  _createClass(ProfileLog, [{
+    key: 'clear',
+    value: function clear() {
+      this.entries = [];
+      this.groups = {};
+    }
+
+    /**
+     * Add profiling measurement info entry if 'enabled'.
+     *
+     * @param since {number} Date.now() to be used to calculate
+     *            elapsed millisecs
+     * @param message {string} description of measurement
+     */
+
+  }, {
+    key: 'addEntry',
+    value: function addEntry(since, message) {
+      if (this.enabled) {
+        var elapsed = since ? ('      ' + (Date.now() - since)).slice(-6) : '';
+        this.entries.push(elapsed + ': ' + message);
+      }
+    }
+
+    /**
+     * Add an elapsed time into the group list.
+     *
+     * @param since {number} Date.now() to be used to calculate
+     *            elapsed millisecs
+     * @param group {string} group name
+     */
+
+  }, {
+    key: 'addToGroup',
+    value: function addToGroup(since, group) {
+      if (!this.groups[group]) this.groups[group] = [];
+      this.groups[group].push(Date.now() - since);
+    }
+
+    /**
+     * @returns {string} formatted content of ProfileLog entries
+     */
+
+  }, {
+    key: 'toString',
+    value: function toString() {
+      var output = this.title + '\n  ' + this.entries.join("\n  ") + '\n';
+      for (var propertyName in this.groups) {
+        if (this.groups.hasOwnProperty(propertyName)) {
+          var group = this.groups[propertyName];
+          output += '\n  ' + propertyName + ':\n';
+          output += '    count: ' + group.length + '\n';
+          output += '    min:   ' + Math.min.apply(Math, group) + '\n';
+          output += '    max:   ' + Math.max.apply(Math, group) + '\n';
+          var sum = group.reduce(function (a, b) {
+            return a + b;
+          });
+          output += '    ave:   ' + (sum / group.length).toFixed(0) + '\n';
+        }
+      }
+      return output;
+    }
+
+    /**
+     * Write ProfileLog entries to console if 'enabled'.
+     */
+
+  }, {
+    key: 'writeToConsole',
+    value: function writeToConsole() {
+      var clear = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+
+      if (this.enabled) {
+        console.log(this.toString());
+        if (clear) this.clear();
+      }
+    }
+  }]);
+
+  return ProfileLog;
+}();
+
+exports.default = ProfileLog;
---
+++ src/profilelog.js
@@ -0,0 +1,96 @@
+'use strict'
+
+/**
+ * Simple profile logging utility
+ *
+ * usage:
+ *   const ProfileLog = require('./profilelog')
+ *   profileLog = new ProfileLog('Test Performance')
+ *   let start = Date.now()
+ *   ...do something being measured...
+ *   profileLog.addEntry(start, 'Did something slow')
+ *   start = Date.now()
+ *   ...do something being measured again...
+ *   profileLog.addEntry(start, 'Did something slow again')
+ *   profileLog.writeToConsole()
+ *
+ * output:
+ *   Test Performance:
+ *      11185: Did something slow
+ *       2089: Did something slow again
+ */
+export default class ProfileLog {
+
+  constructor(title = "Execution Profiling Log:", enabled = true) {
+    this.title = title
+    this.enabled = enabled
+    this.entries = []
+    this.groups = {}
+  }
+
+  /**
+   * Clear the profiling info entries
+   */
+  clear() {
+    this.entries = []
+    this.groups = {}
+  }
+
+  /**
+   * Add profiling measurement info entry if 'enabled'.
+   *
+   * @param since {number} Date.now() to be used to calculate
+   *            elapsed millisecs
+   * @param message {string} description of measurement
+   */
+  addEntry(since, message) {
+    if (this.enabled) {
+      const elapsed = since ? ('      ' + (Date.now() - since)).slice(-6) : ''
+      this.entries.push(`${elapsed}: ${message}`)
+    }
+  }
+
+  /**
+   * Add an elapsed time into the group list.
+   *
+   * @param since {number} Date.now() to be used to calculate
+   *            elapsed millisecs
+   * @param group {string} group name
+   */
+  addToGroup(since, group) {
+    if (!this.groups[group])
+      this.groups[group] = []
+    this.groups[group].push(Date.now() - since)
+  }
+
+  /**
+   * @returns {string} formatted content of ProfileLog entries
+   */
+  toString() {
+    let output = `${this.title}\n  ${this.entries.join("\n  ")}\n`
+    for (let propertyName in this.groups) {
+      if (this.groups.hasOwnProperty(propertyName)) {
+        const group = this.groups[propertyName]
+        output += `\n  ${propertyName}:\n`
+        output += `    count: ${group.length}\n`
+        output += `    min:   ${Math.min.apply(Math, group)}\n`
+        output += `    max:   ${Math.max.apply(Math, group)}\n`
+        const sum = group.reduce((a,b) => a + b)
+        output += `    ave:   ${(sum / group.length).toFixed(0)}\n`
+      }
+    }
+    return output
+  }
+
+  /**
+   * Write ProfileLog entries to console if 'enabled'.
+   */
+  writeToConsole(clear = false) {
+    if (this.enabled) {
+      console.log(this.toString())
+      if (clear)
+        this.clear()
+    }
+  }
+
+}
